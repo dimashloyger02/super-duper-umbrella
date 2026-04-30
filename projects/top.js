07e482ca45d47a0495912c644e52920b681b8273
dimashloyger02 <94590260+dimashloyger02@users.noreply.github.com>
2026-05-01 00:07:20 +0700

    Update top.js

--- projects/top.js
+++ projects/top.js
@@ -1,7 +1,15 @@
 {
   "players": [
-    {"name": "Holdik", "steps": 0},
-    {"name": "Bezsmertny", "steps": 0},
+    {"name": "Бот 1", "steps": 0},
+    {"name": "Бот 2", "steps": 0},
+    {"name": "Бот 3", "steps": 0},
+    {"name": "Бот 4", "steps": 0},
+    {"name": "Бот 5", "steps": 0},
+    {"name": "Бот 6", "steps": 0},
+    {"name": "Бот 7", "steps": 0},
+    {"name": "Бот 8", "steps": 0},
+    {"name": "Бот 9", "steps": 0},
+    {"name": "Бот 10", "steps": 0},
     {"name": "Ты", "steps": 0}
   ]
 }
